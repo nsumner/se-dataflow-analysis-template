c291b0fb6039a2da315e67c51e0ca6f3a68bb708
Nick Sumner <wsumner@sfu.ca>
2019-11-23 12:35:30 -0800

    Update for convenience and compatibility with Java 9+

--- src/main/java/ca/sfu/cmpt745/ex06/checker/KittenCheckerMain.java
+++ src/main/java/ca/sfu/cmpt745/ex06/checker/KittenCheckerMain.java
@@ -3,10 +3,12 @@ package ca.sfu.cmpt745.ex06.checker;
 import soot.Body;
 import soot.PackManager;
 import soot.Transform;
+import soot.options.Options;
 
 
 public class KittenCheckerMain {
   public static void main(String[] args) {
+    Options.v().set_keep_line_number(true);
     Transform checker = new Transform("jtp.KittenChecker", new KittenChecker());
     PackManager.v().getPack("jtp").add(checker);
     soot.Main.main(args);
